d30660bd355c303c194b2275b010d4e174fcc15c
Omar Abdullah <Malfucious97@gmail.com>
2022-07-05 18:17:12 -0500

    The purpose of this commit is to parse the users message

---
+++ src/components/chatbot/MessageParser.js
@@ -0,0 +1,20 @@
+class MessageParser {
+  constructor(actionProvider, state) {
+    this.actionProvider = actionProvider;
+    this.state = state;
+  }
+
+  parse(message) {
+    if (message.includes("hello")) {
+      this.actionProvider.handleHello();
+    }
+    if (message.includes("howdy")) {
+      this.actionProvider.handleHello();
+    }
+    if (message.includes("dog")) {
+      this.actionProvider.handleDog();
+    }
+  }
+}
+
+export default MessageParser;
