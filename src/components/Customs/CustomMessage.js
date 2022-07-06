a59bdcdaac3d0acdde1f862fd5c2678fc1eea1a2
Omar Abdullah <Malfucious97@gmail.com>
2022-07-06 09:32:13 -0500

    Customize messages to display in the bot

---
+++ src/components/Customs/CustomMessage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CustomMessage = () => {
+  return (
+    <img
+      src="https://st2.depositphotos.com/5087307/10133/v/950/depositphotos_101335550-stock-illustration-wow-phrase-vector-design-element.jpg"
+      style={{ width: "100%" }}
+      alt="ping"
+    />
+  );
+};
+export default CustomMessage;
