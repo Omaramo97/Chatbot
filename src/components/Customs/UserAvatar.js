0fce2eb9e54347ba2fcf096168ed0a5f41d1c0bc
Omar Abdullah <Malfucious97@gmail.com>
2022-07-05 19:51:34 -0500

    Image placement for Users

---
+++ src/components/Customs/UserAvatar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserAvatar = () => {
+  return (
+    <img
+      style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+      src="https://cdn.imgbin.com/3/2/10/imgbin-super-mario-bros-new-super-mario-bros-super-mario-run-mario-bros-0nAFuCFYdQNphTXrue1JHXXZ0.jpg"
+      alt="Mario"
+    />
+  );
+};
+
+export default UserAvatar;
