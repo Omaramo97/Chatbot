8e85b646b4323b2d506ecbecc8a0b1258d4f9fd1
Omar Abdullah <Malfucious97@gmail.com>
2022-07-06 09:22:06 -0500

    Bot image

---
+++ src/components/Customs/CustomAvatar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Avatar = () => {
+  return (
+    <img
+      style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+      src="https://icon-library.com/images/orc-icon/orc-icon-6.jpg"
+      alt="orc"
+    />
+  );
+};
+
+export default Avatar;
